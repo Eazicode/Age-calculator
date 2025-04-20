2d2712484efca95f592ef2d60b9132e2f9e73f7a
Eazicode <mistereze16@gmail.com>
2025-04-20 12:47:37 +0100

    The Age Calculator almost done

---
+++ index.js
@@ -0,0 +1,62 @@
+let inputField = document.querySelectorAll('.input-style');
+let displayError = document.querySelector('.display-error');
+let label = document.querySelectorAll('.label');
+let inputYear = document.querySelector('#js-year');
+let inputMonth = document.querySelector('#js-month')
+let inputDate = document.querySelector('#js-date')
+let clearYear = document.querySelectorAll('.js-span-year'); 
+let clearMonth = document.querySelectorAll('.js-span-month')
+let displayYear = document.querySelector('.js-display-year')
+let displayMonth = document.querySelector('.js-display-month')
+
+function fillInputField() {
+    for (let i = 0; i < inputField.length && label.length; i++) {
+      if (inputField[i].value === '') {
+        inputField[i].style.border = '1px solid red';
+        label[i].style.color = 'red'
+        displayError.innerHTML = 'This field are/is required';
+      } 
+       if (inputYear.value !== '')  {
+        inputYear.style.border = 'rgb(206, 205, 205)';
+        label[2].style.color = 'hsl(0, 1%, 44%)';
+        calcualateYear();
+      } 
+       if (inputMonth.value !== '') {
+        inputMonth.style.border = 'rgb(206, 205, 205)';
+        label[1].style.color = 'hsl(0, 1%, 44%)'
+        calcualateMonth()
+      } 
+       if (inputDate.value !== '') {
+        inputDate.style.border = 'rgb(206, 205, 205)';
+        label[0].style.color = 'hsl(0, 1%, 44%)'
+      }
+    }
+
+}
+
+
+function calcualateYear() {
+  let year = new Date().getFullYear();
+  if (inputYear.value !== '') {
+    for (let i = 0; i < clearYear.length; i++) {
+      clearYear[i].style.display = 'none'
+      displayYear.innerHTML = `${year - inputYear.value}`
+    }
+  }
+}
+
+function calcualateMonth() {
+  let month = new Date().getMonth()
+  let monthAnswer = (inputMonth.value - 1) - month;
+
+  if (inputMonth !== '') {
+    for(let i = 0; i < clearMonth.length; i++) {
+      clearMonth[i].style.display = 'none'
+      displayMonth.innerHTML = `${monthAnswer }`
+
+      if (monthAnswer <  0) {
+        displayMonth.innerHTML = Math.abs(monthAnswer)
+      }
+    }
+  }
+}
